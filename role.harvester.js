eff464e9ded56a1616d731178cbbdc82e14c3266
Ross Keddy <ross.keddy1@gmail.com>
2022-06-06 03:28:27 -0400

    Create role.harvester.js

---
+++ role.harvester.js
@@ -0,0 +1,19 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        
+	    var sources = creep.room.find(FIND_SOURCES)
+
+        // Find the closest source to the creep
+        const closestSource = creep.pos.findClosestByRange(sources)
+
+        // Try to harvest the source. If it isn't in range
+        if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+            // Move to it
+            creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+        }
+	}
+};
+
+module.exports = roleHarvester;
